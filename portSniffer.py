fb0d719d1c9b340a77eb6dc173c9a54ff2103b0e
StevenMcGinnis <81644391+Steven-McGinnis@users.noreply.github.com>
2024-02-01 15:52:33 -0500

    Update portSniffer.py

--- portSniffer.py
+++ portSniffer.py
@@ -2,15 +2,13 @@ import socket
 from threading import Thread, Semaphore
 import time
 
-successful_ports = []
-
-def check_ip(ip, port, sem):
-    global successful_ports
+def check_port(ip, port, sem, successful_ports):
     try:
-        with socket.create_connection((ip, port), timeout=1) as sock:
+        with socket.create_connection((ip, port), timeout=5) as sock:
             successful_ports.append(port)
-    except Exception:
-        pass
+            sock.close()
+            print(f"{port} Successfully Identified")
+   
     finally:
         sem.release()
 
@@ -18,20 +16,27 @@ def main():
     host_ip = "192.168.0.100"
     max_threads = 20
     sem = Semaphore(max_threads)
+    successful_ports = []
+    threads = []
 
-    for i in range(7000):
-        sem.acquire()  
-        t = Thread(target=check_ip, args=(host_ip, i, sem,))
-        t.start()
-        
-        if i == 6999:
-            time.sleep(5)
+    for port in range(1, 2002):
+    	sem.acquire()
+    	t = Thread(target=check_port, args=(host_ip, port, sem, successful_ports))
+    	t.start()
+    	threads.append(t)
+    	
+    	
+    for t in threads:
+    	t.join()
         
     print("Successful ports:")
     sorted_ports = sorted(successful_ports)
-    for port in sorted_ports:
+    for port in successful_ports:
         print(port)
 
+def showStart():
+    print("Starting Port Scan")
 
 if __name__ == "__main__":
+    showStart()
     main()
